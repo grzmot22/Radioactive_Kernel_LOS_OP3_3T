75028e8c809e56fb2b6e2e8c80177dd103af7018
Slawek <yarpiin@gmail.com>
2017-01-28 14:38:26 +0000

    add Alucard gov
    # Conflicts:
    #	arch/arm64/configs/radioactive_defconfig
    #	drivers/cpufreq/Kconfig
    #	drivers/cpufreq/cpufreq.c
    #	include/linux/cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -108,7 +108,6 @@ struct cpufreq_policy {
 	 *     __cpufreq_governor(data, CPUFREQ_GOV_POLICY_EXIT);
 	 */
 	struct rw_semaphore	rwsem;
-
 	unsigned int util;
 
 	/* Synchronization for frequency transitions */
@@ -263,6 +262,10 @@ struct cpufreq_driver {
 
 	/* should be defined, if possible */
 	unsigned int	(*get)(unsigned int cpu);
+
+	/* optional */
+	unsigned int (*getavg)	(struct cpufreq_policy *policy,
+                            unsigned int cpu);
 
 	/* optional */
 	int		(*bios_limit)(int cpu, unsigned int *limit);
@@ -474,6 +477,7 @@ struct cpufreq_governor {
 	struct module		*owner;
 };
 
+
 /* Pass a target to the cpufreq driver */
 int cpufreq_driver_target(struct cpufreq_policy *policy,
 				 unsigned int target_freq,
@@ -481,6 +485,8 @@ int cpufreq_driver_target(struct cpufreq_policy *policy,
 int __cpufreq_driver_target(struct cpufreq_policy *policy,
 				   unsigned int target_freq,
 				   unsigned int relation);
+extern int __cpufreq_driver_getavg(struct cpufreq_policy *policy,
+                                   unsigned int cpu);
 int cpufreq_register_governor(struct cpufreq_governor *governor);
 void cpufreq_unregister_governor(struct cpufreq_governor *governor);
 
@@ -536,6 +542,9 @@ extern struct cpufreq_governor cpufreq_gov_dynamic_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BIOSHOCK)
 extern struct cpufreq_governor cpufreq_gov_bioshock;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_bioshock)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
+extern struct cpufreq_governor cpufreq_gov_alucard;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_alucard)
 #endif
 
 /*********************************************************************
