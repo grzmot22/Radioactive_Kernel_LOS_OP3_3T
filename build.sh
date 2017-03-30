3b42365117b124536263dec671d0d5db0406757b
Grzmot22 <grzmot22@gmail.com>
2017-03-30 19:22:22 +0100

    Change path
    Change-Id: I71da12d7b1172f8aad406c7d0a87111a242285b0

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ rm .version
 make mrproper
 make clean
 # Kernel Details
-VER="_V1.2.5_oneplus3-3T"
+VER="_V1.2.4_oneplus3-3T"
 BASE_AK_VER="Radioactive"
 
 # Vars
@@ -13,14 +13,14 @@ export KBUILD_BUILD_USER=acuicultor
 export KBUILD_BUILD_HOST=NuclearPowerPlant
 
 #paths
-KERNEL_DIR="/home/acuicultor/android/kernels/rebase/Radioactive_Kernel_LOS_OP3_3T"
-ZIP_MOVE="${HOME}/android/releases"
-ZIMAGE_DIR="/home/acuicultor/android/kernels/rebase/Radioactive_Kernel_LOS_OP3_3T/arch/arm64/boot"
-MODULES_DIR="${HOME}/android/lazyflasher/modules"
+KERNEL_DIR="/run/media/grzmot22/58b717ea-9909-499b-95aa-12b8c4d0f82f/cm14.1/kernel/oneplus/radioActive"
+ZIP_MOVE="/run/media/grzmot22/58b717ea-9909-499b-95aa-12b8c4d0f82f/android/releases"
+ZIMAGE_DIR="/run/media/grzmot22/58b717ea-9909-499b-95aa-12b8c4d0f82f/cm14.1/kernel/oneplus/radioActive/arch/arm64/boot"
+MODULES_DIR="/run/media/grzmot22/58b717ea-9909-499b-95aa-12b8c4d0f82f/cm14.1/kernel/oneplus/lazyflasher"
 
 #build info 
 THREAD="-j$(grep -c ^processor /proc/cpuinfo)"
-export CROSS_COMPILE=${HOME}/Android/toolchains/ubernaro-aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE=/run/media/grzmot22/58b717ea-9909-499b-95aa-12b8c4d0f82f/android/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 AK_VER="$BASE_AK_VER$VER"
 export LOCALVERSION=~`echo $AK_VER`
 export LOCALVERSION=~`echo $AK_VER`
@@ -29,8 +29,8 @@ make $DEFCONFIG
 make $THREAD
 
 #make our zips
-cp -vr $ZIMAGE_DIR/Image.gz-dtb ~/android/lazyflasher/zImage
+cp -vr $ZIMAGE_DIR/Image.gz-dtb /run/media/grzmot22/58b717ea-9909-499b-95aa-12b8c4d0f82f/android/lazyflasher/zImage
 find $KERNEL_DIR -name '*.ko' -exec cp -v {} $MODULES_DIR \;
-cd ~/android/lazyflasher
+cd /run/media/grzmot22/58b717ea-9909-499b-95aa-12b8c4d0f82f/cm14.1/kernel/oneplus/lazyflasher
 make VERSION=$VER NAME=$BASE_AK_VER
 cd $KERNEL_DIR
